f133faf8f6efa72ec26270ebc2dd6ade55333997
Route9Library <you@example.com>
2020-12-08 17:21:48 -0500

    class prompt 4-9

--- prompt.js
+++ prompt.js
@@ -40,10 +40,10 @@ whiskers.color = "white";
 whiskers["gender"] = "female";
 
 // Prompt 10: Add a property called size with a value of small using dot notation.
-whiskers.size = "small"
+whiskers.size = "small";
 
 // Prompt 11: Add a property called diet with a value of kitty mix using bracket notation.
-whiskers["diet"] = "kitty mix"
+whiskers["diet"] = "kitty mix";
 
 // Classes Prompts
 
@@ -62,7 +62,7 @@ class Smartphone {
      }
   }
 
-//   Prompt 3: Create 1 new instance named phone1 for the Smartphone class.
+// Prompt 3: Create 1 new instance named phone1 for the Smartphone class.
 class Smartphone {
     constructor(OS, model, app_market, company) { 
         this.OS = OS;
@@ -72,4 +72,114 @@ class Smartphone {
      }
   }
 
-  const phone1 = new Smartphone('ios, iphone 12, app store, apple')
+  const phone1 = new Smartphone('ios, iphone 12, app store, apple');
+
+// Prompt 4: Create another new instance named phone2 for the Smartphone class, make it a different device from phone1.
+class Smartphone {
+    constructor(OS, model, app_market, company) { 
+        this.OS = OS;
+        this.model = model;
+        this.app_market = app_market;
+        this.company = company;
+     }
+  }
+
+  const phone1 = new Smartphone('ios', 'iphone 12', 'app store', 'apple');
+
+  const phone2 = new Smartphone('android', 'Galaxy S20 FE 5G', 'play store', 'Samsung');
+
+//  Prompt 5: Print the OS property of phone1 to the console using dot notation.
+class Smartphone {
+    constructor(OS, model, app_market, company) { 
+        this.OS = OS;
+        this.model = model;
+        this.app_market = app_market;
+        this.company = company;
+     }
+  }
+
+  const phone1 = new Smartphone('ios', 'iphone 12', 'app store', 'apple');
+
+  const phone2 = new Smartphone('android', 'Galaxy S20 FE 5G', 'play store', 'Samsung');
+  
+phone1.OS;
+
+// Prompt 6: Print the model property of phone2 to the console using bracket notation.
+class Smartphone {
+    constructor(OS, model, app_market, company) { 
+        this.OS = OS;
+        this.model = model;
+        this.app_market = app_market;
+        this.company = company;
+     }
+  }
+
+  const phone1 = new Smartphone('ios', 'iphone 12', 'app store', 'apple');
+
+  const phone2 = new Smartphone('android', 'Galaxy S20 FE 5G', 'play store', 'Samsung');
+  
+phone2["model"];
+
+// Prompt 7: Add a method called Call to the Smartphone class. This means you will also have to add another parameter called ringtone to the constructor to hold the value/sound the call method would make.
+class Smartphone {
+    constructor(OS, model, app_market, company, ringtone) { 
+        this.OS = OS;
+        this.model = model;
+        this.app_market = app_market;
+        this.company = company;
+        this.ringtone = ringtone;
+     }
+
+     call () {
+    console.log(this.ringtone);  
+     }
+
+  }
+
+  const phone1 = new Smartphone('ios', 'iphone 12', 'app store', 'apple', 'beep beep');
+
+  const phone2 = new Smartphone('android', 'Galaxy S20 FE 5G', 'play store', 'Samsung', 'Mmm Mmm');
+
+//   Prompt 8: Use dot notation to call the call method for phone1 and print it to the console.
+class Smartphone {
+    constructor(OS, model, app_market, company, ringtone) { 
+        this.OS = OS;
+        this.model = model;
+        this.app_market = app_market;
+        this.company = company;
+        this.ringtone = ringtone;
+     }
+
+     call () {
+    console.log(this.ringtone);  
+     }
+
+  }
+
+  const phone1 = new Smartphone('ios', 'iphone 12', 'app store', 'apple', 'beep beep');
+
+  const phone2 = new Smartphone('android', 'Galaxy S20 FE 5G', 'play store', 'Samsung', 'Mmm Mmm');
+
+  phone1.call();
+
+//   Prompt 9: Use bracket notation to call the call method for phone2 and print it to the console.
+class Smartphone {
+    constructor(OS, model, app_market, company, ringtone) { 
+        this.OS = OS;
+        this.model = model;
+        this.app_market = app_market;
+        this.company = company;
+        this.ringtone = ringtone;
+     }
+
+     call () {
+    console.log(this.ringtone);  
+     }
+
+  }
+
+  const phone1 = new Smartphone('ios', 'iphone 12', 'app store', 'apple', 'beep beep');
+
+  const phone2 = new Smartphone('android', 'Galaxy S20 FE 5G', 'play store', 'Samsung', 'Mmm Mmm');
+
+  phone2["call"]();
